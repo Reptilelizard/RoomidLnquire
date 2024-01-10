b8c3a16e8d85fe54d0974b241f6493f2f7b6c115
Reptilelizard <1345lxh@gmail.com>
2024-01-10 23:27:17 +0800

    1.对变量名进行了修改 2.添加错误信息的输出 3.修改注释信息

--- src/main/java/Web.java
+++ src/main/java/Web.java
@@ -4,12 +4,12 @@ import cn.hutool.json.JSONArray;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Web {
     private String extractKey(String url){
-        String keyValue = url.substring(url.lastIndexOf("/") + 1,url.lastIndexOf("."));
-        return  keyValue;
+        return url.substring(url.lastIndexOf("/") + 1,url.lastIndexOf("."));
     }
     public String[] getWbiKey(){
         String nav= HttpUtil.get("https://api.bilibili.com/x/web-interface/nav");
@@ -34,25 +34,27 @@ public class Web {
         midArray.toArray(mid);
         return mid;
     }
-    public String[] getRoomId(String[] mid,String cookie) {
+    public String[] getRoomId(String[] uid,String cookie) {
         Wbi wbi = new Wbi();
         List<String> idArray = new ArrayList<>();
         String[] wbi_img = getWbiKey();
-        for (String m : mid) {
-            String wbi_key = wbi.count(wbi_img, m);
-            String url = "https://api.bilibili.com/x/space/wbi/acc/info?mid=" + m + "&token=&platform=web&web_location=1550101" + wbi_key;
+        for (String mid : uid) {
+            String wbi_key = wbi.count(wbi_img, mid);
+            String url = "https://api.bilibili.com/x/space/wbi/acc/info?mid=" + mid + "&token=&platform=web&web_location=1550101" + wbi_key;
             String userList = HttpRequest.get(url).cookie(cookie).execute().body();
             JSONObject list = JSONUtil.parseObj(userList);
             String code = list.get("code").toString();
-            if (code.equals("-352")) continue;//检查是否被B站风控
+            if (code.equals("-352")) {//检查是否被B站风控
+                System.out.println("Cookie已失效请重新获取");
+                break;
+            }
             if (code.equals("-404")) continue;//检查是否为空用户ID
             JSONObject data = (JSONObject) list.get("data");
             String live = data.get("live_room").toString();
-            if (live.equals("null")) continue;//检查用户是否拥有直播间信息
+            if (live.equals("null")) continue;//检查用户直播间信息是否为空
             JSONObject live_room = (JSONObject) data.get("live_room");
             String id = live_room.get("roomid").toString();
             idArray.add(id);
-
         }
         String[] roomId = new String[idArray.size()];
         idArray.toArray(roomId);
