404ea4be283d99d86d936e1eb65bbe64ade67d84
Reptilelizard <1345lxh@gmail.com>
2024-01-13 00:28:57 +0800

    优化了Config类的变量返回方法

--- src/main/java/Config.java
+++ src/main/java/Config.java
@@ -3,11 +3,12 @@ import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
-
 
 public class Config {
+    public ConfigValue<String,String> getValue(String fileLocation){
+        String cookieValue = readCookie(fileLocation);
+        return getCookie(cookieValue);
+    }
 
     public String readCookie(String fileLocation){
         String cookie = null;
@@ -24,7 +25,7 @@ public class Config {
         return cookie;
     }
 
-    public Map<String, String> getCookie(String cookieJson){
+    public ConfigValue getCookie(String cookieJson){
         JSONObject entries = new JSONUtil().parseObj(cookieJson);
         JSONObject cookies_info = (JSONObject) entries.get("cookie_info");
         JSONArray cookies = (JSONArray) cookies_info.get("cookies");
@@ -35,10 +36,17 @@ public class Config {
         }
         JSONObject token_info = (JSONObject) entries.get("token_info");
         String access_tokenValue = (String) token_info.get("access_token");
-        Map<String,String> cookie = new HashMap<>();
-        cookie.put("access_token",access_tokenValue);
-        cookie.put("cookie",finalCookie);
-        return cookie;
+        return new ConfigValue(access_tokenValue,finalCookie);
     }
 
+}
+
+class ConfigValue<A,B>{
+    public final A access_token;
+    public final B cookie;
+
+    public ConfigValue(A access_token,B cookie){
+        this.access_token = access_token;
+        this.cookie = cookie;
+    }
 }
